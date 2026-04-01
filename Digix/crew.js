88d9d9788d7b6d6b60ce2de853e57c81d77324ae
laurensalexis23-star <laurensalexis23@gmail.com>
2026-03-31 23:14:11 -0400

    Update crew.js

--- Digix/crew.js
+++ Digix/crew.js
@@ -111,7 +111,7 @@ async function connectToWhatsapp(handleMessage) {
                 }
 
                 console.log(`🔄 Requesting pairing code for ${number}`);
-                const code = await sock.requestPairingCode(number, 'DIGICREW');
+                const code = await sock.requestPairingCode(number, 'SUKUNABOT');
                 console.log('📲 Pairing Code:', code);
                 console.log('👉 Enter this code on your WhatsApp app to pair.');
 
