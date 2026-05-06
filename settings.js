7846aad5f9fb4ef9b54093c9aa713ce78be861aa
SUKUNA XD <laurensalexis23@gmail.com>
2026-05-06 15:07:41 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    owner: "GOJO DTH",
+    botName: "GOJO MD V2",
+    menuImage: "https://i.ibb.co/Qvh9hNmV/RD323233353234303838353040732e77686174736170702e6e6574-944612.jpg"
+};
